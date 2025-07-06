1d8a23d05be3ebb54fefa23cde1e47ea7dd2bc03
TuHM <tuhm.enactusftu@gmail.com>
2025-07-06 10:42:47 +0700

    Update

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,21 @@
+// Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDuTvBn8Xl01DYddVXQ7M0L24K3l-GyG0c",
-    authDomain: "enactusftuhanoi-tracuu.firebaseapp.com",
-    projectId: "enactusftuhanoi-tracuu",
-    storageBucket: "enactusftuhanoi-tracuu.appspot.com",
-    messagingSenderId: "611356979403",
-    appId: "1:611356979403:web:2c9a4cffb2b323ce3deb4e"
+  apiKey: "AIzaSyDuTvBn8Xl01DYddVXQ7M0L24K3l-GyG0c",
+  authDomain: "enactusftuhanoi-tracuu.firebaseapp.com",
+  projectId: "enactusftuhanoi-tracuu",
+  storageBucket: "enactusftuhanoi-tracuu.appspot.com",
+  messagingSenderId: "611356979403",
+  appId: "1:611356979403:web:2c9a4cffb2b323ce3deb4e"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Shortcut các service
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 
 // Tạo collection references
-const usersCollection = db.collection("users");
-const candidatesCollection = db.collection("candidates");
-const roundsCollection = db.collection("recruitmentRounds");
-const activitiesCollection = db.collection("activities");
+const usersRef = db.collection("users");
+const candidatesRef = db.collection("candidates");
+const roundsRef = db.collection("rounds");
